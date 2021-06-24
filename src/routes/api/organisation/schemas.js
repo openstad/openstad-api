a585d5b2b397cd3f5d5097be47fb1a049cc01b69
Daan Rosbergen <d.rosbergen@gmail.com>
2021-06-24 10:43:58 +0200

    fix: add trim to phone numbers, allow trailing slash in instagram url

--- src/routes/api/organisation/schemas.js
+++ src/routes/api/organisation/schemas.js
@@ -4,7 +4,7 @@ exports.createOrganisation = Joi.object({
   name: Joi.string().required(),
   street: Joi.string().max(2048),
   zip: Joi.string().regex(/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-zA-Z]{2}$/),
-  phone: Joi.string().max(10),
+  phone: Joi.string().max(10).trim(),
   district: Joi.string().required(),
   email: Joi.string().email().required(),
   website: Joi.string().uri(),
@@ -19,7 +19,7 @@ exports.createOrganisation = Joi.object({
   instagram: Joi.string()
     .uri()
     .regex(
-      /https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/,
+      /https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,30}(?:[A-Za-z0-9_]))?)(\/)?/,
       {
         invert: true,
       }
@@ -31,12 +31,12 @@ exports.createOrganisation = Joi.object({
   contactName: Joi.string().required(),
   contactPosition: Joi.string().required(),
   contactEmail: Joi.string().email().required(),
-  contactPhone: Joi.string().max(10).required(),
+  contactPhone: Joi.string().max(10).trim().required(),
 
   // Municipality contact details
   municipalityContactName: Joi.string().required(),
   municipalityContactEmail: Joi.string().email(),
-  municipalityContactPhone: Joi.string().min(10).max(10),
+  municipalityContactPhone: Joi.string().min(10).max(10).trim(),
   municipalityContactStatement: Joi.string(),
 }).options({ stripUnknown: true });
 
@@ -45,7 +45,7 @@ exports.updateOrganisation = Joi.object({
   street: Joi.string().max(2048),
   zip: Joi.string().regex(/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-zA-Z]{2}$/),
   district: Joi.string(),
-  phone: Joi.string().max(10),
+  phone: Joi.string().trim().max(10),
   email: Joi.string().email(),
   website: Joi.string().uri(),
   facebook: Joi.string()
@@ -59,7 +59,7 @@ exports.updateOrganisation = Joi.object({
   instagram: Joi.string()
     .uri()
     .regex(
-      /https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/,
+      /https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,30}(?:[A-Za-z0-9_]))?)/,
       {
         invert: true,
       }
