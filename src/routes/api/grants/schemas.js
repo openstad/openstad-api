a7af32279118c56aa4ac51071c5daabcb36f485a
Daan Rosbergen <d.rosbergen@gmail.com>
2021-09-09 14:48:40 +0200

    fix(grant): allow emty url for grants

--- src/routes/api/grants/schemas.js
+++ src/routes/api/grants/schemas.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 
 exports.createGrant = Joi.object({
     name: Joi.string().required(),
-    url: Joi.string().uri()
+    url: Joi.string().allow(null, '').uri()
 }).options({ stripUnknown: true })
 
 exports.updateGrant = Joi.object({
     name: Joi.string(),
-    url: Joi.string().uri()
+    url: Joi.string().allow(null, '').uri()
 }).options({ stripUnknown: true })
