764eda6bde3372b54f6e8aba2ed4c3b1c21d728d
Niels Vegter <niels@denes.nl>
2021-11-03 14:30:13 +0000

    Fix migration SQL

--- migrations/030-cleanup-unused-fields.js
+++ migrations/030-cleanup-unused-fields.js
@@ -4,11 +4,11 @@ module.exports = {
   up: function() {
     try {
       return db.query(`
-        DROP TABLE meetings;
-        ALTER TABLE articles DROP duration;
         ALTER TABLE articles DROP endDate;
-        ALTER TABLE ideas DROP duration;
         ALTER TABLE ideas DROP endDate;
+        ALTER TABLE ideas DROP INDEX meetingId;
+        ALTER TABLE ideas DROP meetingId;
+        DROP TABLE meetings;
 			`);
     } catch(e) {
       return true;
