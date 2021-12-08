be5cb4aaac6dcffa2b52acd0b1a05ae8e768b218
Niels Vegter <niels@denes.nl>
2021-12-08 16:03:10 +0000

    Fix auth for Vote - related ticket https://trello.com/c/y974bnEH

--- src/models/Vote.js
+++ src/models/Vote.js
@@ -116,10 +116,10 @@ module.exports = function( db, sequelize, DataTypes ) {
     listableBy: 'all',
     viewableBy: 'all',
     createableBy: 'member',
-    updateableBy: ['editor', 'owner'],
-    deleteableBy: ['editor', 'owner'],
+    updateableBy: ['moderator', 'owner'],
+    deleteableBy: ['moderator', 'owner'],
     canToggle: function(user, self) {
-      return userHasRole(user, 'editor', self.userId);
+      return userHasRole(user, 'moderator', self.userId);
     }
   }
 
